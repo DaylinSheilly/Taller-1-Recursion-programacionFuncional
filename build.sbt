9f3c88bb8d505de4ac864046c08bbd5fed772837
Sheilly Ortega 2040051-3743 <sheilly.ortega@correounivalle.edu.co>
2022-04-30 18:39:09 -0500

    Created the project

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Taller1"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
---
+++ src/main/scala/Recursion/package.scala
@@ -0,0 +1,14 @@
+package object Recursion {
+  /**
+   * Ejercicio 1
+   */
+
+  /**
+   * Ejercicio 2
+   */
+
+  /**
+   * Ejercicio 3
+   */
+
+}
